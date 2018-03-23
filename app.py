ef28ea00db72a1e0d25545f1a0506ce24eea2d27
Nicholas Yang Chee <nicholas.yang.chee@gmail.com>
2018-03-22 22:15:50 -0400

    Tax calculation complete

--- app.py
+++ app.py
@@ -2,6 +2,29 @@ import flask
 app = flask.Flask(__name__)
 
 
-@app.route("/")
+@app.route('/tax', methods=['POST'])
+def tax():
+    print("In tax")
+    # Assume incoming data is JSON
+    data = flask.request.get_json(force=True)
+    print(data)
+
+    # Find city
+    state = data['shippingAddress']['state']
+
+    # Find total value
+    total = data['total']
+    tax = 0
+
+    # If city is NY, apply 5% tax
+    if state == 'NY':
+        tax = int(total * 0.05)
+
+    resp = {'tax': tax}
+    return flask.jsonify(resp)
+
+
+@app.route('/')
 def main():
-  return flask.render_template('app.html', words="YO")
+    print("In main")
+    return flask.render_template('app.html', words='YO')
