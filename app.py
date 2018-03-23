23019ba93494e12c3c37123d466f3eeab485126f
Nicholas Yang Chee <nicholas.yang.chee@gmail.com>
2018-03-23 13:44:57 -0400

    Completed exercise base requirements

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import flask
 app = flask.Flask(__name__)
 
 
+# Calculates tax based on shipping address
 @app.route('/tax', methods=['POST'])
 def tax():
-    print("In tax")
+    print('In tax')
     # Assume incoming data is JSON
     data = flask.request.get_json(force=True)
     print(data)
@@ -24,7 +25,30 @@ def tax():
     return flask.jsonify(resp)
 
 
+# Returns an array of shipping options
+@app.route('/shipping', methods=['POST'])
+def shipping():
+    print('In shipping')
+
+    resp = []
+
+    twoDayShipping = {
+      'typeId': 'two day',
+      'cost': 5000,
+      'type': 'Two-Day Shipping'
+    }
+    overnightShipping = {
+      'typeId': 'overnight',
+      'cost': 10000,
+      'type': 'Overnight Shipping'
+    }
+
+    resp.append(twoDayShipping)
+    resp.append(overnightShipping)
+
+    return flask.jsonify(resp)
+
+
 @app.route('/')
 def main():
-    print("In main")
-    return flask.render_template('app.html', words='YO')
+    return flask.render_template('app.html')
